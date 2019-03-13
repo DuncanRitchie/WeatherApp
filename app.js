612af1eaf09023b2d4a67116b3a477021fd970a1
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-13 17:28:27 +0000

    Changing the input to test getWeather().

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 const request = require("request");
-weatherUrl = "https://api.darksky.net/forecast/be7deb5ceb49b65e140c2bddebbe9f06/53.1921,-2.8803?units=si";
-geoUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Chester.json?access_token=pk.eyJ1IjoiZHVuY2FuciIsImEiOiJjanQ3ZGZ2OXgwb3NxM3pydXUzbmN1MThjIn0.Hw9zenxA4-uRV32LPm7Iqw"
 
-request({url: weatherUrl, json: true},(error, response)=>{
-    if (error) {
-        console.log("Error getting weather data!")
-    }
-    else {
-        let data = response.body;
-        console.log(data.currently)
-    }
-})
+const getWeather = (address) => {
+    geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiZHVuY2FuciIsImEiOiJjanQ3ZGZ2OXgwb3NxM3pydXUzbmN1MThjIn0.Hw9zenxA4-uRV32LPm7Iqw`
+    request({url: geoUrl, json: true},(error, geoResponse)=>{
+        if (error) {
+            console.log("Error getting geo-data! Maybe your internet's down, or the location doesn't exist.")
+        }
+        else {
+            let geodata = geoResponse.body;
+            let longitude = geodata.features[0].center[0];
+            let latitude = geodata.features[0].center[1];
+            if (latitude>=0) {
+                northOrSouth = "North"
+            }
+            else {
+                northOrSouth = "South"
+            }
+            if (longitude>=0) {
+                eastOrWest = "East"
+            }
+            else {
+                eastOrWest = "West"
+            }
+            console.log(`The latitude of ${geodata.features[0].place_name} is ${Math.abs(latitude)} degrees ${northOrSouth} and the longitude is ${Math.abs(longitude)} degrees ${eastOrWest}.`);
+            weatherUrl = `https://api.darksky.net/forecast/be7deb5ceb49b65e140c2bddebbe9f06/${latitude},${longitude}?units=si`;
+            request({url: weatherUrl, json: true},(error, weatherResponse)=>{
+                if (error || weatherResponse == undefined) {
+                    console.log("Error getting weather data!")
+                }
+                else {
+                    console.log(weatherResponse.body.currently)
+                }
+            })
+        }
+    })
+}
 
-request({url: geoUrl, json: true},(error, response)=>{
-    if (error) {
-        console.log("Error getting geo-data!")
-    }
-    else {
-        let data = response.body;
-        console.log(`The longitude of Chester is ${data.features[0].geometry.coordinates[0]} degrees and the latitude is ${data.features[0].geometry.coordinates[1]} degrees.`)
-    }
-})
+getWeather("Ile Kerguelen")

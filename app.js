ab8f62ee1bd5ca367a36ca8bdfd97e29947e0c0e
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-14 16:14:16 +0000

    Now requests WhereOnEarth from Flickr.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const request = require("request");
 const chalk = require("chalk");
+// const JSO = require("jso");
+
+const flickrKey = "75c23a119862573eb7204058a885d80b";
+// const flickrSecret = "d902fa4a08de9ab6";
 
 const getWeather = (address) => {
     geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiZHVuY2FuciIsImEiOiJjanQ3ZGZ2OXgwb3NxM3pydXUzbmN1MThjIn0.Hw9zenxA4-uRV32LPm7Iqw`
@@ -31,10 +35,20 @@ const getWeather = (address) => {
                 }
                 else {
                     console.log(weatherResponse.body.currently);
+                    flickrPlaceUrl = `https://api.flickr.com/services/rest/?method=flickr.places.findByLatLon&api_key=${flickrKey}&lat=${latitude}&lon=${longitude}&accuracy=4&format=json`;
+                    request({url: flickrPlaceUrl},(error, flickrPlaceResponse)=>{
+                        if (error || flickrPlaceResponse == undefined) {
+                            console.log("Error getting place_id from Flickr!")
+                        }
+                        else {
+                            let woe = JSON.parse(flickrPlaceResponse.body.slice(14,-1)).places.place[0].woeid;
+                            console.log("Flickr says your place response is "+woe);
+                        }
+                    })
                 }
             })
         }
     })
 }
 
-getWeather("Melbourne")
+getWeather("Chester")

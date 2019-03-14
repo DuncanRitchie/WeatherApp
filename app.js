5e34bc4bde9c4773dfd5565276a1c0c9647a1a94
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-14 11:10:36 +0000

    Installed Chalk.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const chalk = require("chalk");
 
 const getWeather = (address) => {
     geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiZHVuY2FuciIsImEiOiJjanQ3ZGZ2OXgwb3NxM3pydXUzbmN1MThjIn0.Hw9zenxA4-uRV32LPm7Iqw`
@@ -22,18 +23,18 @@ const getWeather = (address) => {
             else {
                 eastOrWest = "West"
             }
-            console.log(`The latitude of ${geodata.features[0].place_name} is ${Math.abs(latitude)} degrees ${northOrSouth} and the longitude is ${Math.abs(longitude)} degrees ${eastOrWest}.`);
+            console.log(chalk.green(`${chalk.bold(geodata.features[0].place_name)} is ${Math.abs(latitude)} degrees ${northOrSouth} and ${Math.abs(longitude)} degrees ${eastOrWest}.`));
             weatherUrl = `https://api.darksky.net/forecast/be7deb5ceb49b65e140c2bddebbe9f06/${latitude},${longitude}?units=si`;
             request({url: weatherUrl, json: true},(error, weatherResponse)=>{
                 if (error || weatherResponse == undefined) {
                     console.log("Error getting weather data!")
                 }
                 else {
-                    console.log(weatherResponse.body.currently)
+                    console.log(weatherResponse.body.currently);
                 }
             })
         }
     })
 }
 
-getWeather("Ile Kerguelen")
+getWeather("Melbourne")
